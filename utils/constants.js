4a802af16162b33c84c290f629b7849aaba2eec8
seif_elsayed <seifsayed121@gmail.com>
2026-07-14 17:57:28 +0300

    constants

---
+++ utils/constants.js
@@ -0,0 +1,13 @@
+exports.SUPER_ADMIN = "SUPER_ADMIN";
+exports.ADMIN = "ADMIN";
+exports.BUYER = "BUYER";
+exports.SELLER = "SELLER";
+exports.BORKER = "BORKER";
+exports.COMPANY = "COMPANY";
+
+
+exports.ROLES = [exports.SUPER_ADMIN, exports.ADMIN, exports.BUYER, exports.SELLER, exports.BORKER, exports.COMPANY];
+exports.LANGS = ["EN", "AR"];
+
+exports.LOGIN_TYPE = ["APPLE", "GOOGLE", "EMAIL", "SOCIAL"];
+exports.GENDER = ["MALE", "FEMALE"];
